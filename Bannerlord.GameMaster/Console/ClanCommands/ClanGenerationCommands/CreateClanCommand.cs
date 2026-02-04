7fcd51d89ab9101275176b99188f14da807171cc
SolWayward <jared-wyatt@outlook.com>
2026-02-04 14:20:07 -0500

    Added optional culture arg to create_clan command

--- Bannerlord.GameMaster/Console/ClanCommands/ClanGenerationCommands/CreateClanCommand.cs
+++ Bannerlord.GameMaster/Console/ClanCommands/ClanGenerationCommands/CreateClanCommand.cs
@@ -4,6 +4,7 @@ using Bannerlord.GameMaster.Console.Common.EntityFinding;
 using Bannerlord.GameMaster.Console.Common.Formatting;
 using Bannerlord.GameMaster.Console.Common.Parsing;
 using Bannerlord.GameMaster.Console.Common.Validation;
+using Bannerlord.GameMaster.Cultures;
 using System.Collections.Generic;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Library;
@@ -14,10 +15,10 @@ namespace Bannerlord.GameMaster.Console.ClanCommands.ClanGenerationCommands
     public static class CreateClanCommand
     {
         /// <summary>
-		/// Create a clan with the specified name. Optionally set a hero as leader and assign to kingdom.
-		/// Usage: gm.clan.create_clan &lt;clanName&gt; [leaderHero] [kingdom] [createParty] [companionCount]
-		/// </summary>
-		[CommandLineFunctionality.CommandLineArgumentFunction("create_clan", "gm.clan")]
+  /// Create a clan with the specified name. Optionally set a hero as leader and assign to kingdom.
+  /// Usage: gm.clan.create_clan &lt;clanName&gt; [leaderHero] [kingdom] [createParty] [companionCount] [culture]
+  /// </summary>
+  [CommandLineFunctionality.CommandLineArgumentFunction("create_clan", "gm.clan")]
         public static string CreateClan(List<string> args)
         {
             return Cmd.Run(args, () =>
@@ -27,7 +28,7 @@ namespace Bannerlord.GameMaster.Console.ClanCommands.ClanGenerationCommands
                     return error;
 
                 string usageMessage = CommandValidator.CreateUsageMessage(
-                    "gm.clan.create_clan", "<clanName> [leaderHero] [kingdom] [createParty] [companionCount]",
+                    "gm.clan.create_clan", "<clanName> [leaderHero] [kingdom] [createParty] [companionCount] [culture]",
                     "Create a new clan with the specified name. If no leader is specified, a new hero will be generated.\n" +
                     "Optionally, specify a kingdom for clan, if no kingdom specified, clan is independent.\n" +
                     "- clanName/name: required, name for the clan. Use SINGLE QUOTES for multi-word names\n" +
@@ -35,12 +36,13 @@ namespace Bannerlord.GameMaster.Console.ClanCommands.ClanGenerationCommands
                     "- kingdom: optional, kingdom ID or name for clan to join (independent if omitted)\n" +
                     "- createParty/party: optional, 'true' or 'false' to create party for leader (default: true)\n" +
                     "- companionCount/companions: optional, number of companions to add (0-10, default: 2)\n" +
-                    "Supports named arguments: name:'The Highland Clan' leader:derthert kingdom:empire party:true companions:5",
+                    "- culture/cultures: optional, culture pool for auto-generated leader. Use commas with no spaces for multiple (default: main_cultures)\n" +
+                    "Supports named arguments: name:'The Highland Clan' leader:derthert kingdom:empire party:true companions:5 culture:vlandia",
                     "gm.clan.create_clan Highlanders\n" +
                     "gm.clan.create_clan 'The Highland Clan' derthert\n" +
                     "gm.clan.create_clan NewClan myHero empire\n" +
-                    "gm.clan.create_clan name:'House Stark' kingdom:sturgia party:true companions:5\n" +
-                    "gm.clan.create_clan TradingFamily null null false 0");
+                    "gm.clan.create_clan name:'House Stark' kingdom:sturgia party:true companions:5 culture:sturgia\n" +
+                    "gm.clan.create_clan TradingFamily null null false 0 vlandia,battania");
 
                 ParsedArguments parsed = ArgumentParser.ParseArguments(args);
 
@@ -49,7 +51,8 @@ namespace Bannerlord.GameMaster.Console.ClanCommands.ClanGenerationCommands
                     new ArgumentDefinition("leaderHero", false, null, "leader"),
                     new ArgumentDefinition("kingdom", false),
                     new ArgumentDefinition("createParty", false, null, "party"),
-                    new ArgumentDefinition("companionCount", false, null, "companions")
+                    new ArgumentDefinition("companionCount", false, null, "companions"),
+                    new ArgumentDefinition("culture", false, null, "cultures")
                 );
 
                 string validationError = parsed.GetValidationError();
@@ -99,6 +102,15 @@ namespace Bannerlord.GameMaster.Console.ClanCommands.ClanGenerationCommands
                         return MessageFormatter.FormatErrorMessage(countError);
                 }
 
+                CultureFlags cultureFlags = CultureFlags.AllMainCultures;
+                string cultureArg = parsed.GetArgument("culture", 5) ?? parsed.GetArgument("cultures", 5);
+                if (cultureArg != null)
+                {
+                    cultureFlags = FlagParser.ParseCultureArgument(cultureArg);
+                    if (cultureFlags == CultureFlags.None)
+                        return MessageFormatter.FormatErrorMessage($"Invalid culture(s): '{cultureArg}'. Use culture names (e.g., vlandia,battania) or groups (main_cultures, bandit_cultures, all_cultures)");
+                }
+
                 if (!CommandValidator.ValidateClanCreationLimit(1, out string clanLimitError))
                     return MessageFormatter.FormatErrorMessage(clanLimitError);
 
@@ -116,10 +128,11 @@ namespace Bannerlord.GameMaster.Console.ClanCommands.ClanGenerationCommands
                     { "leaderHero", leader != null ? leader.Name.ToString() : "Auto-generated" },
                     { "kingdom", kingdom != null ? kingdom.Name.ToString() : "Independent" },
                     { "createParty", createParty.ToString() },
-                    { "companionCount", companionCount.ToString() }
+                    { "companionCount", companionCount.ToString() },
+                    { "culture", cultureArg ?? "Main Cultures" }
                 };
 
-                Clan newClan = ClanGenerator.CreateNobleClan(clanName, leader, kingdom, createParty, companionCount);
+                Clan newClan = ClanGenerator.CreateNobleClan(clanName, leader, kingdom, createParty, companionCount, cultureFlags);
 
                 string leaderInfo = leader != null ? $" with {leader.Name} as leader" : " with auto-generated leader";
                 string kingdomInfo = kingdom != null ? $" and joined {kingdom.Name}" : " as independent";
